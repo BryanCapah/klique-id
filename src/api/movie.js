6b7ebb5eb6c587a7c9d3a70dfda3a8cfee31ea3d
brian capah <briancapah@192.168.1.28>
2021-11-11 09:47:22 +0700

    remove unused stuff

--- src/api/movie.js
+++
@@ -1,7 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from "axios";
-
-export const getMovie = createAsyncThunk('movie/getMovies', async ({ page = 1, keyword = 'Batman' }) => {
-    const data = await axios.get(`https://www.omdbapi.com?apikey=d492a296&s=${keyword}&page=${page}`)
-    return data?.data
-})
