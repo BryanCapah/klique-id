a9c26eb92a0d7bc80a43a3425b6e7ee7068ba387
brian capah <briancapah@root.local>
2021-11-16 00:37:31 +0700

    cleaning up

---
+++ build/static/js/4.088b20b5.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpt=this.webpackJsonpt||[]).push([[4],{160:function(t,n,e){"use strict";e.r(n),e.d(n,"default",(function(){return c}));var i=e(13);function c(){return Object(i.jsx)("div",{children:"detail"})}}}]);
+//# sourceMappingURL=4.088b20b5.chunk.js.map
